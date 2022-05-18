2cc3fa200288f07c39f54692de7c00f175c690d6
AbbieK805 <97373292+AbbieK805@users.noreply.github.com>
2022-05-17 20:45:31 -0500

    Adding remaining stub routes

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,21 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.send('GET /places')
+router.get('/', (req,res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/thai-food.jpeg'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: '/images/cat-coffee.jpeg'
+      }]
+      
+    res.render('places/index', {  places  })
 })
 
 module.exports = router
