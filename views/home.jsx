9c5f139fc27b9fe34aabfd8b67e086fdbc7da8cf
AbbieK805 <97373292+AbbieK805@users.noreply.github.com>
2022-05-17 21:31:23 -0500

    Adding home page view

---
+++ views/home.jsx
@@ -0,0 +1,23 @@
+const React = require('react')
+const Def = require('./default')
+
+function home() {
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                    <img src="/images/sandwich.jpg" alt="a delicious sandwich" />
+                    <div>
+                        Photo by Ria Quartz on Unsplash
+                    </div>
+                </div>
+                <a href="/places">
+                    <button className="btn btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = home
