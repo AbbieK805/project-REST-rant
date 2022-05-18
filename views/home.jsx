bab3a9c487cd1fed88c810f0f7f5aea17ba615de
AbbieK805 <97373292+AbbieK805@users.noreply.github.com>
2022-05-17 22:40:21 -0500

    Update home.jsx

--- views/home.jsx
+++ views/home.jsx
@@ -4,18 +4,18 @@ const Def = require('./default')
 function home() {
     return (
         <Def>
-            <main>
-                <h1>HOME</h1>
-                <div>
-                    <img src="/images/sandwich.jpg" alt="a delicious sandwich" />
+                <main>
+                    <h1>HOME</h1>
                     <div>
-                        Photo by Ria Quartz on Unsplash
+                        <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                        <div>
+                            Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                        </div>
                     </div>
-                </div>
-                <a href="/places">
-                    <button className="btn btn-primary">Places Page</button>
-                </a>
-            </main>
+                    <a href="/places">
+                        <button className="btn-primary">Places Page</button>
+                    </a>
+                </main>
         </Def>
     )
 }
