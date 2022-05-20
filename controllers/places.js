9727a16842d9d209fc4f9d2a812f717bf92e2f5a
AbbieK805 <97373292+AbbieK805@users.noreply.github.com>
2022-05-19 21:01:59 -0500

    Update places.js

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,49 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req,res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai-food.jpeg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cat-coffee.jpeg'
-      }]
-      
     res.render('places/index', {  places  })
+})
+
+router.get('/new', (req,res) => {
+    res.render('places/new')
+})
+
+router.get('/:id', (req,res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]){
+        res.render('error404')
+    } else {
+        res.render('places/show', {  place: places[id], id  })
+    }
+})
+
+router.post('/', (req, res) => {
+    if (!req.body.pic) {
+        req.body.pic = 'https://placekitten.com/200/300'
+    }
+    if (!req.body.city) {
+        req.body.city = "Anytown"
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+})
+
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]) {
+        res.render('error404')
+    } else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
 })
 
 module.exports = router
